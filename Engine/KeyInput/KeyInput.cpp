292d7ac6717bb0ded451a893cc936fe32fddc9c4
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-06-23 14:32:52 +0900

    KeyInputClassを作成

---
+++ Engine/KeyInput/KeyInput.cpp
@@ -0,0 +1,23 @@
+#include "KeyInput.h"
+#include <cassert>
+
+std::array<BYTE, 0x100> KeyInput::key = { 0 };
+std::array<BYTE, 0x100> KeyInput::preKey = { 0 };
+
+void KeyInput::Input() {
+	std::copy(key.begin(), key.end(), preKey.begin());
+	// ƒL[“ü—Í
+	if (!GetKeyboardState(key.data())) {
+		assert(!"Input Failed");
+	}
+}
+
+bool KeyInput::Pushed(int32_t keyType) {
+	return (key[keyType] & 0x80) && !(preKey[keyType] & 0x80);
+}
+bool KeyInput::LongPush(int32_t keyType) {
+	return (key[keyType] & 0x80) && (preKey[keyType] & 0x80);
+}
+bool KeyInput::Releaed(int32_t keyType) {
+	return !(key[keyType] & 0x80) && (preKey[keyType] & 0x80);
+}
--- Engine/KeyInput/KeyInput.h
+++ Engine/KeyInput/KeyInput.h
@@ -1,6 +1,22 @@
 #pragma once
+#include <array>
+#include <Windows.h>
 
 class KeyInput {
-	
+private:
+	KeyInput() = default;
+	~KeyInput() = default;
 
+public:
+	static void Input();
+
+	static bool Pushed(int32_t keyType);
+	static bool LongPush(int32_t keyType);
+	static bool Releaed(int32_t keyType);
+
+
+private:
+	// キー入力バッファー
+	static std::array<BYTE, 0x100> key;
+	static std::array<BYTE, 0x100> preKey;
 };
